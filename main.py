bfbfc5bb97561713cc547c7d7e075654894598ed
Yu Zhang <EthanWang@Yus-MacBook-Air.local>
2018-11-25 23:50:30 -0500

    install certificate

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hi")
+#-----------------------------------------------------------------------------
+# Name:        Discord_Bot (python.py)
+# Purpose:     To be a bot
+#
+# Author:      Ethan Wang
+# Created:     24-Nov-2018
+# Updated:     24-Nov-2018
+#-----------------------------------------------------------------------------
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+bot = commands.Bot(command_prefix='?')
+
+@bot.event
+async def on_ready():
+    print("My name is " + bot.user.name)
+
+bot.run("NTE1OTg5OTAwNTQ2NTM5NTMw.Dt0Lqw.6AWESViOUJv8Fom5Hb_9Dm46Y8A")
