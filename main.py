8d6e8ee8ae079dbb228d9d3a0026c2f2d6b34cd7
Yu Zhang <EthanWang@Yus-MacBook-Air.local>
2018-11-28 00:25:53 -0500

    wait for message command

--- main.py
+++ main.py
@@ -19,11 +19,12 @@ TOKEN = os.environ['TOKEN']
 
 frames_path = '/Users/EthanWang/Discord_Bot/frames.txt'
 
-
 @bot.event
 async def on_ready():
     print("My name is " + bot.user.name)
     await bot.change_presence(game=discord.Game(name="with sand", type=1))
+
+down_votes = ['project management', 'compsci', 'comp sci', 'emerging tech', 'main program', 'culminating', 'exam']
 
 @bot.event
 async def on_message(message):
@@ -33,6 +34,10 @@ async def on_message(message):
     # Help centre
     if input.startswith("?help"):
         await bot.send_message(message.channel, "kys")
+
+    # Downvote
+    if any(x in input for x in down_votes):
+        await bot.add_reaction(message, '👎')
 
     # Prevent overriding
     await bot.process_commands(message)
@@ -55,15 +60,15 @@ async def on_message_delete(message):
 
 
 @bot.command()
-async def surgery(arg1):
+async def surgery(object):
     # Surgery meme
-    first_letter = arg1[0:1]
+    first_letter = object[0:1]
     if first_letter in ['a', 'e', 'i', 'o']:
-        await bot.say("they did surgery on an " + arg1)
+        await bot.say("they did surgery on an " + object)
     elif first_letter == '@':
-        await bot.say("they did surgery on " + arg1)
+        await bot.say("they did surgery on " + object)
     else:
-        await bot.say("they did surgery on a " + arg1)
+        await bot.say("they did surgery on a " + object)
 
 @bot.command()
 async def say(arg1):
@@ -133,12 +138,22 @@ async def kms():
 
 
 @bot.command()
-async def bomb(start):
-    count = int(start)
+async def bomb(start_time):
+    count = int(start_time)
     while count > 0:
-        await bot.say(count)
+        await bot.say(":bomb:{}".format(count))
         count -= 1
         time.sleep(1)
     await bot.say("boom.")
+
+
+@bot.command()
+async def shoot(member: discord.Member):
+    await bot.say("{},:gun: you've been shot at! Quick, type 'duck'".format(member.mention))
+    msg = await bot.wait_for_message(timeout=2, author=member, content='duck')
+    if msg:
+        await bot.say("whew, that was a close one")
+    else:
+        await bot.say("You've been killed! you snooze you lose")
 
 bot.run(TOKEN)
